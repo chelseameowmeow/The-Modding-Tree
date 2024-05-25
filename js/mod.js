5adc675156500a97fea90cd6ce86a8305fc4b5a6
chelseameowmeow <claudia.greyling@gmail.com>
2024-05-25 18:53:32 +0200

    Update mod.js

--- js/mod.js
+++ js/mod.js
@@ -1,6 +1,6 @@
 let modInfo = {
-	name: "The ??? Tree",
-	id: "mymod",
+	name: "The Pony Magic Tree",
+	id: "PonyMagicTree",
 	author: "nobody",
 	pointsName: "points",
 	modFiles: ["layers.js", "tree.js"],
